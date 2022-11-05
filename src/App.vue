ad72e9a6da3020ce3ea3761db6fc33dd795fba91
JaryJay <47123100+JaryJay@users.noreply.github.com>
2022-11-04 20:26:19 -0400

    Make to-do list app

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-for="task of tasks">
+    <Task :text="task" />
+  </div>
+  <input type="text" v-model="newTaskName">
+  <button @click="addTask">
+    Add
+  </button>
+</template>
+
+<script>
+import Task from './components/Task.vue';
+export default {
+  data() {
+    return {
+      tasks: ["do the laundry", "vaccuum the floor", "walk the dog", "register for clockhacks", "apsdhfpasdh"],
+      username: "Jay",
+      username2: "bob",
+      numClicks: 0,
+      newTaskName: "",
+    }
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.newTaskName);
+      this.newTaskName = "";
+    }
+  },
+  components: {
+    Task
+  }
+}
+</script>
+
+<style>
+#jay {
+  color: green;
+}
+
+button {
+  padding: 3px;
+}
+
+button:hover {
+  padding: 5px;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="task" :class="done ? 'done' : ''">
+		<input type="checkbox" v-model="done">
+		{{ text }}!!
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			// textVar: "some placeholder text",
+			done: false
+		}
+	},
+	props: {
+		text: String
+	}
+}
+</script>
+
+<style>
+#task {
+	border: 1px;
+	background-color: white;
+	padding: 10px;
+	border-radius: 5px;
+	color: black;
+	margin: 5px;
+}
+
+.done {
+	text-decoration: line-through;
+	opacity: 0.5;
+}
+</style>
